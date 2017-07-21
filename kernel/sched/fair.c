5ae1e0b9844e44d0617ebdc897b52cf887372f64
Joel Fernandes <joelaf@google.com>
2017-07-20 23:46:56 -0700

    sched/fair: Skip frequency updates if CPU about to idle
    If CPU is about to idle, prevent a frequency update. With the number of
    schedutil governor wake ups are reduced by more than half on a test
    playing bluetooth audio.
    
    Test: sugov wake ups drop by more than half when playing music with
    screen off (476 / 1092)
    
    Bug: 64689959
    
    Change-Id: I400026557b4134c0ac77f51c79610a96eb985b4a
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>
    Signed-off-by: pqhaz <usaguy1342@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4471,6 +4471,7 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 
 #define UPDATE_TG	0x0
 #define SKIP_AGE_LOAD	0x0
+#define SKIP_CPUFREQ	0x3
 
 static inline void update_load_avg(struct sched_entity *se, int not_used1){}
 static inline void
@@ -5891,6 +5892,9 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
 
+	if (task_sleep && rq->nr_running == 1)
+		flags |= DEQUEUE_IDLE;
+
 #ifdef CONFIG_SMP
 	/*
 	 * Update SchedTune accounting
@@ -5940,7 +5944,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (cfs_rq_throttled(cfs_rq))
 			break;
 
-		update_load_avg(se, UPDATE_TG);
+		update_load_avg(se, UPDATE_TG | (flags & DEQUEUE_IDLE));
 		update_cfs_shares(se);
 	}
 
